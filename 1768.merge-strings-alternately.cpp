df86a1d7f07620ceccd804c3671363b2d4a9394a
BlitzMK <sec180005@utdallas.edu>
2023-10-04 20:58:25 -0400

    Q1

--- 1768.merge-strings-alternately.cpp
+++ 1768.merge-strings-alternately.cpp
@@ -3,12 +3,24 @@
  *
  * [1768] Merge Strings Alternately
  */
+#include <string>
 
+using namespace std;
 // @lc code=start
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        
+        string result = "";
+        for(int i = 0; i<word1.length() || i <word2.length(); i++){
+            if(i<word1.length()){
+                result.append(1,word1[i]);
+            }
+            if(i<word2.length()){
+                result.append(1,word2[i]);
+            }
+        }
+
+        return(result);
     }
 };
 // @lc code=end
